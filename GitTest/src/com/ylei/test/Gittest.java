a1ba52152f24a4a12e6bc67db83f957d5e8901ca
Y.Lei <ilostforever@163.com>
2014-04-29 17:58:40 +0800

    add list

--- GitTest/src/com/ylei/test/Gittest.java
+++ GitTest/src/com/ylei/test/Gittest.java
@@ -1,8 +1,24 @@
 package com.ylei.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Gittest {
 	public static void main(String[] args) {
 		System.out.println("Git Test");
 		System.out.println("Git branch meila");
+		List<String> lists = Gittest.addlist();
+		for (String list : lists) {
+			System.out.println(list);
+		}
+		
 	}
+public static List<String> addlist(){
+	
+	List<String> list = new ArrayList<String>();
+	for (int i = 0; i < 20; i++) {
+		 list.add(i, "100"+i);	
+	}
+	return list;
+}
 }
